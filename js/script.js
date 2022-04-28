8b3115c4e48005aa37c50a4e053e74a4c4d88efb
kingsleyetukdo <kingsleyj90@gmail.com>
2022-04-28 15:57:56 +0100

    last change

--- js/script.js
+++ js/script.js
@@ -15,23 +15,19 @@ function loadDoc(e) {
         if (this.status === 200) {
             const dataInput = JSON.parse(this.responseText) 
             ipAddress.innerHTML = dataInput.ip
-            locationData.innerHTML = `${dataInput.location.region}, ${dataInput.location.city}`
+            locationData.innerHTML = `${dataInput.location.region}, ${dataInput.location.city} ${dataInput.as.asn}`
             timeZone.innerHTML = dataInput.location.timezone
             isp.innerHTML = dataInput.isp
             const latitude = dataInput.location.lat
             const longitude = dataInput.location.lng
-
-            // console.log(latitude,longitude)
-
+                console.log(dataInput)
             let map = L.map('map').setView([latitude,longitude], 13);
-            
             
             let greenIcon = L.icon({
                 iconUrl: '/images/icon-location.svg',
                 iconSize:     [30, 50], // size of the icon
             });
            
-
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
@@ -39,52 +35,44 @@ function loadDoc(e) {
         L.marker([longitude, latitude], {icon: greenIcon}).addTo(map)
             .bindPopup('welcome')
             .openPopup();
-            
-            // console.log(dataInput)
         }
         
-        }
+}
         
     // xhttp.send();
-    document.querySelector('#col').value = ''
+    // document.querySelector('#col').value = ''
     e.preventDefault()
     }
 
-    
-
-
-
-    function json(url) {
-        return fetch(url).then(res => res.json());
-      }
+    // function json(url) {
+    //     return fetch(url).then(res => res.json());
+    //   }
       
     //   let apiKey = 'at_GWKPWwKCvCVcKYj3AYZsOV9oUDEo9';
     //   json(`https://geo.ipify.org/api/v2/country,city?apiKey=at_GWKPWwKCvCVcKYj3AYZsOV9oUDEo9&ipAddress`).then(data => {
 
-        ipAddress.innerHTML = data.ip
-        locationData.innerHTML = `${data.location.region}, ${data.location.country}`
-        timeZone.innerHTML = data.location.timezone
-        isp.innerHTML = data.isp
-        const latitude = data.location.lat
-        const longitude = data.location.lng
+    //     ipAddress.innerHTML = data.ip
+    //     locationData.innerHTML = `${data.location.region}, ${data.location.country} ${data.as.asn}`
+    //     timeZone.innerHTML = data.location.timezone
+    //     isp.innerHTML = data.isp
+    //     const latitude = data.location.lat
+    //     const longitude = data.location.lng
 
-
-        let map = L.map('map').setView([longitude, latitude], 13);
-
-            
-        let greenIcon = L.icon({
-            iconUrl: '/images/icon-location.svg',
-            iconSize:     [30, 50], // size of the icon
-        });
+    //     let map = L.map('map').setView([longitude, latitude], 13);
+ 
+    //     let greenIcon = L.icon({
+    //         iconUrl: '/images/icon-location.svg',
+    //         iconSize:     [30, 50], // size of the icon
+    //     });
         
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+    //     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //     }).addTo(map);
         
-        L.marker([latitude, longitude], {icon: greenIcon}).addTo(map)
-            .bindPopup('welcome')
-            .openPopup();
-      });
+    //     L.marker([latitude, longitude], {icon: greenIcon}).addTo(map)
+    //         .bindPopup('welcome')
+    //         .openPopup();
+    //   });
 
 
     
