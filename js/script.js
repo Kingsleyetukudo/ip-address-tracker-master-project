20d5b899b153e2ec97b5ed96cae2a840ffb8bbf8
kingsleyetukdo <kingsleyj90@gmail.com>
2022-04-28 08:32:50 +0100

    almost done

--- js/script.js
+++ js/script.js
@@ -1,40 +1,82 @@
+let ipAddress = document.querySelector('.address-data')
+let locationData = document.querySelector('.location-data')
+let timeZone = document.querySelector('.time-zone-data')
+let isp = document.querySelector('.isp-data')
+document.getElementById('submit').addEventListener('submit', loadDoc)
 
-
-function loadDoc() {
+function loadDoc(e) {
+    const use = document.querySelector('#col').value
+    console.log(use)
     const xhttp = new XMLHttpRequest();
+    
+    xhttp.open("GET", `https://geo.ipify.org/api/v2/country,city?apiKey=at_GWKPWwKCvCVcKYj3AYZsOV9oUDEo9&ipAddress=${use}`, true);
+
     xhttp.onload = function() {
-      document.getElementById("test").innerHTML = this.responseText;
+        if (this.status === 200) {
+            const dataInput = JSON.parse(this.responseText) 
+            ipAddress.innerHTML = dataInput.ip
+            locationData.innerHTML = `${dataInput.location.region}, ${dataInput.location.city}`
+            timeZone.innerHTML = dataInput.location.timezone
+            isp.innerHTML = dataInput.isp
+            const latitude = dataInput.location.lat
+            const longitude = dataInput.location.lng
+
+            let map = L.map('map').setView([longitude, latitude], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([latitude, longitude]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+            
+            // console.log(dataInput)
+        }
+        
+        }
+
+    // xhttp.send();
+    e.preventDefault()
+    }
+
+
+
+    function json(url) {
+        return fetch(url).then(res => res.json());
       }
-    xhttp.open("GET", "ajax_info.txt", true);
-    xhttp.send();
-  }
+      
+    //   let apiKey = 'at_GWKPWwKCvCVcKYj3AYZsOV9oUDEo9';
+    //   json(`https://geo.ipify.org/api/v2/country,city?apiKey=at_GWKPWwKCvCVcKYj3AYZsOV9oUDEo9&ipAddress=129.56.74.238
+    //   `).then(data => {
+
+        ipAddress.innerHTML = data.ip
+        locationData.innerHTML = `${data.location.city}, ${data.location.country}`
+        timeZone.innerHTML = data.location.timezone
+        isp.innerHTML = data.isp
+        const latitude = data.location.lat
+        const longitude = data.location.lng
 
 
+        let map = L.map('map').setView([longitude, latitude], 13);
 
-  // how to call use the IP Address 
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-//   let ip = "8.8.8.8";    
-//   let api_key = "at_GWKPWwKCvCVcKYj3AYZsOV9oUDEo9";    
-//   (function () {       
-//       $.ajax({url: "https://geo.ipify.org/apiv1",       data: {apiKey: api_key, ipAddress: ip},           success: function(data) {("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");  
-//             }       
-//     });    
-//     console.log(data) 
-// });
+        L.marker([latitude, longitude]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+        console.log(data.ip);
+        console.log(data.location.city);
+        console.log(data.location.country);
+        console.log(data.location.timezone);
+        console.log(data.isp);
+        console.log(data.country_code);
+        //so many more properties
+      });
 
 
-
-
-
-let map = L.map('map').setView([6.484742, 3.3529217], 50);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([9.0820, 8.6753]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
 
 
 
